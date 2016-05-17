887be70e5a6c5f95f30552feb2cc443c2e015c6c
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2016-05-17 01:25:25 -0500

    Fix a call that caused a never ending recursive chain, moved to the correct interactor

--- usecases/files.go
+++ usecases/files.go
@@ -12,7 +12,7 @@ func (interactor GHInteractor) CreateFile(file domain.File, author domain.Author
 }
 
 func (interactor GHInteractor) AddFiles(files []domain.File, author domain.Author, username, repo string) error {
-	err := interactor.AddFiles(files, author, username, repo)
+	err := interactor.GithubRepository.AddFiles(files, author, username, repo)
 	if err != nil {
 		return err
 	}
