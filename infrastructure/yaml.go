92c2f25c6b9e4dda82c6a6de62268552d677e2df
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2016-09-16 12:28:02 -0500

    Add YAML field name

--- infrastructure/yaml.go
+++ infrastructure/yaml.go
@@ -13,7 +13,7 @@ type Configuration struct {
 	ClientSecret string   `yaml:"clientSecret"`
 	Salt         string   `yaml:"salt"`
 	Scopes       []string `yaml:"scopes,flow"`
-	APIHost      string
+	APIHost      string   `yaml:"apihost"`
 }
 
 // GetConfiguration returns the configuration stored in a file
